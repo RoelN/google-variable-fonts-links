82f5717cd2b4989b147f02e8c6efe69d225ce1d7
Roel Nieskens <roel@pixelambacht.nl>
2021-03-02 12:52:19 +0100

    Add `ital` axis manually`

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const fetch = require("node-fetch");
 const fontURL = "https://fonts.google.com/metadata/fonts";
 
 fetch(fontURL)
-	.then(res => res.text())
-	.then(originalJSON => {
+	.then((res) => res.text())
+	.then((originalJSON) => {
 		const json = parseJSON(originalJSON);
 		const variableFonts = getVF(json);
 		const allURLs = constructURLs(variableFonts);
@@ -12,31 +12,33 @@ fetch(fontURL)
 		console.log(allURLs);
 	});
 
-
 // Original JSON is invalid as it contains garbage
 // on the first line, so clean up and parse
 const parseJSON = (json) => {
 	let lines = json.split("\n");
 	lines.splice(0, 1);
 	return JSON.parse(lines.join("\n"));
-}
+};
 
 // Return fonts with variable axes
 const getVF = (json) => {
 	return json["familyMetadataList"].filter((f) => f.axes.length > 0);
-}
+};
 
 // Construct URL for all axes to fonts.googleapis.com
 const constructURLs = (fonts) => {
-	const baseURL = "https://fonts.googleapis.com/css2?family="
+	const startURL = "https://fonts.googleapis.com/css2?family=";
+	const endURL = "&display=block";
+
 	let URLs = [];
 
 	for (const font of fonts) {
 		const axes = getAllAxes(font);
+		const ital = hasItal(font);
 
 		// Add font family
 		let fontURL = [];
-		fontURL.push(baseURL);
+		fontURL.push(startURL);
 		fontURL.push(encodeURI(font.family));
 		fontURL.push(":");
 
@@ -47,14 +49,32 @@ const constructURLs = (fonts) => {
 			axisNames.push(axis.tag);
 			axisValues.push(`${axis.min}..${axis.max}`);
 		}
+
+		// Since `ital` is not listed as an axis, add
+		// it manually
+		if (ital) {
+			fontURL.push("ital,");
+		}
+
 		fontURL.push(axisNames.join(","));
 		fontURL.push("@");
+
+		// For fonts with `ital`, list other axes twice,
+		// once for regular and once for italic
+		if (ital) {
+			fontURL.push("0,");
+			fontURL.push(axisValues.join(","));
+			fontURL.push(";1,");
+		}
+
 		fontURL.push(axisValues.join(","));
+
+		fontURL.push(endURL);
 
 		URLs.push(fontURL.join(""));
 	}
 	return URLs;
-}
+};
 
 // Return all variable axes, sorted how the Google API likes it
 const getAllAxes = (font) => {
@@ -72,8 +92,16 @@ const getAllAxes = (font) => {
 			uppercaseAxes.push(axis);
 		}
 	}
-	lowercaseAxes.sort((a, b) => (a.tag > b.tag) ? 1 : -1)
-	uppercaseAxes.sort((a, b) => (a.tag > b.tag) ? 1 : -1)
+	lowercaseAxes.sort((a, b) => (a.tag > b.tag ? 1 : -1));
+	uppercaseAxes.sort((a, b) => (a.tag > b.tag ? 1 : -1));
 
 	return [...lowercaseAxes, ...uppercaseAxes];
-}
+};
+
+// The JSON doesn't list `ital` as an axis, so we have
+// to deduct this from the `fonts` data (e.g. see if
+// styles ending with "i" are present, like "400i")
+const hasItal = (font) => {
+	const italicFonts = Object.keys(font.fonts).filter((f) => f.endsWith("i"));
+	return italicFonts.length > 0;
+};
